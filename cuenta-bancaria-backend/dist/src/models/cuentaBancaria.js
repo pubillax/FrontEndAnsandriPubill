09eb5240c392d9d65ba3061e0d1b29cd6e247f5b
burploom <burpybloom@gmail.com>
2025-04-21 17:38:11 -0300

    ya casi terminado, falta conexion y probar

---
+++ cuenta-bancaria-backend/dist/src/models/cuentaBancaria.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.cuentaBancaria = void 0;
+class cuentaBancaria {
+    constructor(nombre, saldo, activo) {
+        this.nombre = nombre;
+        this.saldo = saldo;
+        this.activo = activo;
+    }
+}
+exports.cuentaBancaria = cuentaBancaria;
